e093c00dca0312a4c9c31f3b9f45bc4da30ae52c
Yurii <titov.y.y@nmu.one>
2025-03-02 01:04:44 +0200

    task07

--- task07/appsync_src/GraphQL_API/resolvers/mutation/createevent/code.js
+++ task07/appsync_src/GraphQL_API/resolvers/mutation/createevent/code.js
@@ -7,16 +7,28 @@ export function request(ctx) {
         return util.error("NotFoundError");
     }
 
+    const currentDate = new Date();
+    const isoString = currentDate.toISOString();
+    console.log(isoString);
+    const id = ctx.stash.id;
+
+    const parsedObject = JSON.parse(ctx.args.input.payLoad);
+    const key1 = parsedObject.meta.key1;
+    const key2 = parsedObject.meta.key2;
+
     return {
             operation: "PutItem",
             key: util.dynamodb.toMapValues({
                 PK: ctx.args.input.userId
             }),
             attributeValues: util.dynamodb.toMapValues({
+                id: id,
                 userId: ctx.args.input.userId,
-                payLoad: ctx.args.input.payLoad,
-                nic: ctx.args.input.nic,
-                personal_info: ctx.args.input.personal_info
+                createdAt: isoString,
+                payLoad:
+                    meta:
+                        key1: key1,
+                        key2: key2
             }),
             condition: {
             }
