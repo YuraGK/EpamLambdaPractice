4c3d54b286480965878770a2b5581e35df8ec6c1
Yurii <titov.y.y@nmu.one>
2025-03-07 09:49:52 +0200

    task09

---
+++ task09/jsrc/main/java/com/task09/ApiHandler.java
@@ -0,0 +1,69 @@
+package com.task09;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayV2HTTPEvent;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayV2HTTPResponse;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.google.gson.Gson;
+import com.syndicate.deployment.annotations.lambda.LambdaHandler;
+import com.syndicate.deployment.annotations.lambda.LambdaLayer;
+import com.syndicate.deployment.annotations.lambda.LambdaUrlConfig;
+import com.syndicate.deployment.model.Architecture;
+import com.syndicate.deployment.model.ArtifactExtension;
+import com.syndicate.deployment.model.DeploymentRuntime;
+import com.syndicate.deployment.model.RetentionSetting;
+import com.syndicate.deployment.model.lambda.url.AuthType;
+import com.lambda.layer.exchange.OpenMeteoSimpleApi;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+@LambdaHandler(
+    lambdaName = "api_handler",
+	roleName = "api_handler-role",
+		layers = {"sdk-layer"},
+	isPublishVersion = true,
+	aliasName = "${lambdas_alias_name}",
+	logsExpiration = RetentionSetting.SYNDICATE_ALIASES_SPECIFIED
+)
+@LambdaLayer(
+		layerName = "sdk-layer",
+		libraries = {"sdct-at-java-lambda_layer/open-meteo-sdk-1.0.0.jar"},
+		runtime = DeploymentRuntime.JAVA11,
+		architectures = {Architecture.ARM64},
+		artifactExtension = ArtifactExtension.ZIP
+)
+@LambdaUrlConfig(
+		authType = AuthType.NONE
+)
+public class ApiHandler implements RequestHandler<APIGatewayV2HTTPEvent, APIGatewayV2HTTPResponse> {
+
+	private final Map<String, String> responseHeaders = Map.of("Content-Type", "application/json");
+
+	private final Gson gson = new Gson();
+	public APIGatewayV2HTTPResponse handleRequest(APIGatewayV2HTTPEvent event, Context context) {
+		if ("GET".equals(event.getRequestContext().getHttp().getMethod()) && "/weather".equals(event.getRawPath())) {
+
+			OpenMeteoSimpleApi weatherApiClient = new OpenMeteoSimpleApi();
+			try {
+				String response = weatherApiClient.getForecast();
+				return buildResponse(200, response);
+			} catch (IOException e) {
+				throw new RuntimeException(e);
+			}
+
+		} else {
+			return buildResponse(400, gson.toJson(new HashMap<>()));
+		}
+	}
+	private APIGatewayV2HTTPResponse buildResponse(int statusCode, String body) {
+		return APIGatewayV2HTTPResponse.builder()
+				.withStatusCode(statusCode)
+				.withHeaders(responseHeaders)
+				.withBody(body)
+				.build();
+	}
+
+}
