742c42abf51af73c033898e4b28c4aed50f6ad30
Yurii <titov.y.y@nmu.one>
2025-03-07 12:36:00 +0200

    task10.start

---
+++ task10/jsrc/main/java/com/task10/processor.java
@@ -0,0 +1,87 @@
+package com.task10;
+
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayV2HTTPEvent;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayV2HTTPResponse;
+import com.syndicate.deployment.annotations.environment.EnvironmentVariable;
+import com.syndicate.deployment.annotations.environment.EnvironmentVariables;
+import com.syndicate.deployment.annotations.lambda.LambdaHandler;
+import com.syndicate.deployment.annotations.lambda.LambdaUrlConfig;
+import com.syndicate.deployment.annotations.resources.DependsOn;
+import com.syndicate.deployment.model.ResourceType;
+import com.syndicate.deployment.model.RetentionSetting;
+import com.amazonaws.services.dynamodbv2.model.AttributeValue;
+
+import com.lambda.layer.exchange.OpenMeteoSimpleApi;
+import com.syndicate.deployment.model.lambda.url.AuthType;
+import com.syndicate.deployment.model.lambda.url.InvokeMode;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
+@LambdaHandler(
+    lambdaName = "processor",
+	roleName = "processor-role",
+	isPublishVersion = true,
+	aliasName = "${lambdas_alias_name}",
+	logsExpiration = RetentionSetting.SYNDICATE_ALIASES_SPECIFIED
+)
+@LambdaUrlConfig(
+		authType = AuthType.NONE,
+		invokeMode = InvokeMode.BUFFERED
+)
+@DependsOn(name = "Weather",
+		resourceType = ResourceType.DYNAMODB_TABLE)
+@EnvironmentVariables(value = {
+		@EnvironmentVariable(key = "region", value = "${region}"),
+		@EnvironmentVariable(key = "table", value = "${target_table}")})
+public class processor implements RequestHandler<APIGatewayV2HTTPEvent, APIGatewayV2HTTPResponse> {
+
+	private final Map<String, String> responseHeaders = Map.of("Content-Type", "application/json");
+
+	public APIGatewayV2HTTPResponse handleRequest(APIGatewayV2HTTPEvent event, Context context) {
+
+		OpenMeteoSimpleApi weatherApiClient = new OpenMeteoSimpleApi();
+		try {
+			String forecast = weatherApiClient.getForecast();
+			Map<String, AttributeValue> itemValues = new HashMap<>();
+			String uuid = UUID.randomUUID().toString();
+
+			itemValues.put("id", new AttributeValue(uuid));
+			itemValues.put("forecast", new AttributeValue(forecast));
+
+			saveToDynamoDb(itemValues);
+
+			String response = "{\n" +
+					"    \"id\": \""+uuid+"\",\n" +
+					"    \"forecast\": "+forecast+" \n" +
+					"}";
+
+			return buildResponse(200, response);
+		} catch (IOException e) {
+			e.printStackTrace();
+			return buildResponse(400, "{\"statusCode\": 400, \"event\": "+ e.getMessage() +"}");
+		}
+
+	}
+
+	private void saveToDynamoDb(Map<String, AttributeValue> itemValues) {
+		final AmazonDynamoDB ddb = AmazonDynamoDBClientBuilder.standard()
+				.withRegion(System.getenv("region"))
+				.build();
+		ddb.putItem(System.getenv("table"), itemValues);
+	}
+
+	private APIGatewayV2HTTPResponse buildResponse(int statusCode, String body) {
+		return APIGatewayV2HTTPResponse.builder()
+				.withStatusCode(statusCode)
+				.withHeaders(responseHeaders)
+				.withBody(body)
+				.build();
+	}
+}
